f9b5bfc2f63c86a62eb1b286010628269440b33c
Daniel Lopez <d.lopez.1740@gmail.com>
2020-05-11 14:51:48 -0400

    Angular Autenticación simple con Nodejs, Mongodb, Express y JWT (Parte 1/2)

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const app = express();
 
 require('./database');
 
+app.use(express.json());
+
 app.use('/api', require('./routes'));
 
 app.listen(3000, () => console.log('Server on port', 3000));
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,10 +3,54 @@ const router = Router();
 
 const User = require('../models/User');
 
+const jwt = require('jsonwebtoken');
+
 router.get('/', (req, res) => res.send('Hello world'));
 
 router.post('/signup', (req, res) => {
-    res.send('Testing');
+    const {email, password} = req.body;
+    const newUser = new User({email, password});
+    newUser.save();
+
+    const token = jwt.sign({_id: newUser._id}, 'secretKey');
+
+    res.status(200).json({token});
+});
+
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if (!user) return res.status(401).send("The email doesn't exists");
+    if (user.password !== password) return res.status(401).send("Wrong Password");
+
+    const token = jwt.sign({_id: user._id}, 'secretKey');
+
+    res.status(200).json({token});
+});
+
+router.get('/tasks', async (req, res) => {
+    res.json([]);
+});
+
+router.get('/private-tasks', verifyToken, async (req, res) => {
+    res.json([]);
+});
+
+router.get('/profile', verifyToken, async (req, res) => {
+    res.json({user_id: req.userId});
 });
 
 module.exports = router;
+
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized Request');
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if (token === null) {
+        return res.status(401).send('Unauthorized Request');
+    }
+    const payload = jwt.verify(token, 'secretKey');
+    req.userId = payload._id;
+    next();
+}
